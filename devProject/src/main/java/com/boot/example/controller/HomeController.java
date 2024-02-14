b5fe0f5ab7424dc7a79a0ad73c131054d5b5cfb0
networkSorcerer <gigattfyhig@gmail.com>
2024-02-14 17:51:37 +0900

    spring

---
+++ devProject/src/main/java/com/boot/example/controller/HomeController.java
@@ -0,0 +1,24 @@
+package com.boot.example.controller;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.Locale;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+	
+	@GetMapping("/")
+	public String home(Locale locale, Model model) {
+		LocalDateTime now = LocalDateTime.now();
+		
+		DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy년 MM월 dd일 (E) a hh시 mm분 ss초");
+		String formattedNow = now.format(formatter);
+		model.addAttribute("serverTime", formattedNow);
+		
+		return "home"; //  /WEB-INF/views/home.jsp
+	}
+}
